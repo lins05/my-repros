3afa4e444f7ddbb6ef019cfe893b76248c96ddd8
Shuai Lin <linshuai2012@gmail.com>
2023-02-06 23:14:28 +0800

    Added a cljs leaf dep

--- repro-shadow-cljs-advanced-renaming-esm/src/app/main.cljs
+++ repro-shadow-cljs-advanced-renaming-esm/src/app/main.cljs
@@ -1,8 +1,12 @@
 (ns app.main
-  (:require ["/app/util_esm.js" :as ESM]))
+  (:require ["/app/util_esm.js" :as ESM]
+            [app.util-cljs :as utils-cljs]))
 
 (ESM/callDogBark #js {:bark (fn []
                                 (println "Bark from ESM!"))} 1)
+
+(utils-cljs/call-dog-bark #js {:bark (fn []
+                                       (println "Bark from ESM!"))} 1)
 
 
 (defn go []
